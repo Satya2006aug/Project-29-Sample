f8911a9faaf9e0e1e279cbb492ac355a26691fa9
Satya2006aug <71299493+Satya2006aug@users.noreply.github.com>
2020-10-31 17:55:22 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+var block1,block2,block3,block4,block5,block6,block7,block8,block9;
+var engine,world;
+
+function setup() {
+  createCanvas(800,400);
+  engine = Engine.create();
+  world = engine.world;
+  block1 = new Box(330,235,30,40);
+  block2 = new Box(360,235,30,40);
+  block3 = new Box(390,235,30,40);
+  block4 = new Box(420,235,30,40);
+  block5 = new Box(450,235,30,40);
+  block6 = new Box(360,195,30,40);
+  block7 = new Box(390,195,30,40);
+  block8 = new Box(420,195,30,40);
+  block9 = new Box(390,155,30,40);
+}
+
+function draw() {
+  background(255,255,255);
+  Engine.update(engine);  
+  block1.display();
+  block2.display();
+  block3.display();
+  block4.display();
+  block5.display();
+  block6.display();
+  block7.display();
+  block8.display();
+  block9.display();
+
+  
+  
+  
+  
+  
+  drawSprites();
+}
